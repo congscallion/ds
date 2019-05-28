605958b3b594ca0c49aad333a48f59cd0a6130d9
wangcong <wangcong@bbdservice.com>
2019-05-28 20:49:05 +0800

    array status demo

---
+++ src/test/java/io/ds/congscallion/array/chat01/ArrayDefineTest.java
@@ -0,0 +1,14 @@
+/**
+ * BrandBigData.com Inc. Copyright (c) 2019 All Rights Reserved.
+ */
+package io.ds.congscallion.array.chat01;
+
+/**
+ *
+ *
+ * @author wangcong
+ * @version $Id: ArrayDefineTest.java, v0.1 5/28/19 8:48 PM wangcong Exp $$
+ */
+public class ArrayDefineTest {
+
+}
