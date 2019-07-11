c93c07ce420783a378f3dc100993a2de4801ad15
wangcong <wangcong@bbdservice.com>
2019-07-11 08:48:16 +0800

    stack linked list

---
+++ src/main/java/io/ds/congscallion/stack/StackAsLinkedList.java
@@ -0,0 +1,84 @@
+/**
+ * BrandBigData.com Inc. Copyright (c) 2019 All Rights Reserved.
+ */
+package io.ds.congscallion.stack;
+
+/**
+ * 使用链表实现Stack
+ *
+ * @author wangcong
+ * @version $Id: StackAsLinkedList.java, v0.1 7/11/19 8:28 AM wangcong Exp $$
+ */
+public class StackAsLinkedList {
+
+    StackNode root;
+
+    static class StackNode {
+
+        int data;
+        StackNode next;
+
+        public StackNode(int data) {
+            this.data = data;
+        }
+
+        public StackNode(int data, StackNode next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+
+    public boolean isEmpty() {
+        return null == root;
+    }
+
+    public void push(int data){
+
+        StackNode newNode = new StackNode(data);
+
+        if(null == root){
+            root = newNode;
+        }else{
+
+            StackNode temp = root;
+            root = newNode;
+            newNode.next = temp;
+
+        }
+
+
+    }
+
+
+    public int pop(){
+
+        if(null == root){
+            return Integer.MIN_VALUE;
+        }else{
+
+            StackNode temp = root;
+
+            root = temp.next;
+
+            return temp.data;
+        }
+
+
+    }
+
+
+    public int peek(){
+        if(null == root){
+            return Integer.MIN_VALUE;
+        }else{
+            return root.data;
+        }
+    }
+
+
+
+
+
+
+}
