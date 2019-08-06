b85477876339df6cd84ded141a5e2c2095d8eb18
wangcong <wangcong@bbdservice.com>
2019-08-06 15:29:59 +0800

    rewrite api

---
+++ src/main/java/io/ds/congscallion/collections/tree/BinaryTreeNode.java
@@ -0,0 +1,16 @@
+/**
+ * BrandBigData.com Inc. 
+ * Copyright (c) 2019 All Rights Reserved.
+ */
+package io.ds.congscallion.collections.tree;
+
+/**
+ *
+ * 一个简单的接口，代表二叉树的一个节点。 该接口定义了二叉树节点通用的方法，并为其它基于二叉树结构的树提供基础。比如： 二叉搜索树、红黑树。
+ *
+ * @author wangcong
+ * @version $Id: BinaryTreeNode.java, v0.1 8/6/19 11:19 AM wangcong Exp $$
+ */
+public interface BinaryTreeNode<E> {
+
+}
