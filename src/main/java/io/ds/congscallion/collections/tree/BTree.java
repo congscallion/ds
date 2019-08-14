e09cd9bd91bc4bebe3b7cb147e55eae4143eacc9
wangcong <wangcong@bbdservice.com>
2019-08-14 18:38:19 +0800

    B-tree demo

---
+++ src/main/java/io/ds/congscallion/collections/tree/BTree.java
@@ -0,0 +1,51 @@
+/**
+ * BrandBigData.com Inc. 
+ * Copyright (c) 2019 All Rights Reserved.
+ */
+package io.ds.congscallion.collections.tree;
+
+/**
+ *
+ *
+ * @author wangcong
+ * @version $Id: BTree.java, v0.1 8/14/19 5:09 PM wangcong Exp $$
+ */
+public class BTree<E extends Comparable> implements Tree {
+
+
+    /**
+     *
+     * 将数据插入B树中
+     *
+     * @param e
+     */
+    public void add(E e) {
+
+    }
+
+
+    /**
+     *
+     * 在树中搜索包含指定数据的节点
+     *
+     * @param e
+     * @return
+     */
+    public E search(E e) {
+
+        return e;
+    }
+
+
+    /**
+     *
+     * 从树中删除指定数据项
+     *
+     * @param e
+     */
+    public void delete(E e) {
+
+    }
+
+
+}
