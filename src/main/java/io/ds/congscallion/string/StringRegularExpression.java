e21fd8520387c80d2dc6bc7314427847311d717c
wangcong <wangcong@bbdservice.com>
2019-08-21 15:45:32 +0800

    regular expression String algorithm  demo

---
+++ src/main/java/io/ds/congscallion/string/StringRegularExpression.java
@@ -0,0 +1,43 @@
+/**
+ * BrandBigData.com Inc. 
+ * Copyright (c) 2019 All Rights Reserved.
+ */
+package io.ds.congscallion.string;
+
+import java.util.regex.Pattern;
+
+/**
+ *
+ *
+ * @author wangcong
+ * @version $Id: CheckStringNumber.java, v0.1 8/21/19 3:18 PM wangcong Exp $$
+ */
+public class StringRegularExpression {
+
+    public static final String DIGIT1 = ".*[^0-9].*";
+    public static final String DIGIT2 = ".*\\D.*";
+
+
+    public static boolean isDigit(String word) {
+
+        return !Pattern.compile(DIGIT1).matcher(word).matches();
+    }
+
+
+    public static boolean isDigit2(String word) {
+        return !Pattern.compile(DIGIT2).matcher(word).matches();
+    }
+
+    public static boolean isDigit3(String word) {
+        return Pattern.compile("^[0-9]*$").matcher(word).matches();
+    }
+
+
+    public static boolean isStringOnlyAlphabet(String str) {
+        return ((!str.equals(""))
+                && (str != null)
+                && (str.matches("^[a-zA-Z]*$")));
+    }
+
+
+}
---
+++ src/test/java/io/ds/congscallion/string/StringRegularExpressionTest.java
@@ -0,0 +1,69 @@
+/**
+ * BrandBigData.com Inc. 
+ * Copyright (c) 2019 All Rights Reserved.
+ */
+package io.ds.congscallion.string;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+/**
+ *
+ *
+ * @author wangcong
+ * @version $Id: StringRegularExpressionTest.java, v0.1 8/21/19 3:26 PM wangcong Exp $$
+ */
+public class StringRegularExpressionTest {
+
+
+    @Test
+    public void testIsDigit1() {
+
+        assertThat(StringRegularExpression.isDigit("123")).isTrue();
+        assertThat(StringRegularExpression.isDigit("40")).isTrue();
+        assertThat(StringRegularExpression.isDigit("-123")).isFalse();
+        assertThat(StringRegularExpression.isDigit("123.12")).isFalse();
+        assertThat(StringRegularExpression.isDigit("123b")).isFalse();
+        assertThat(StringRegularExpression.isDigit("abc123")).isFalse();
+
+    }
+
+
+    @Test
+    public void testIsDigit2() {
+
+        assertThat(StringRegularExpression.isDigit2("123")).isTrue();
+        assertThat(StringRegularExpression.isDigit2("40")).isTrue();
+        assertThat(StringRegularExpression.isDigit2("-123")).isFalse();
+        assertThat(StringRegularExpression.isDigit2("123.12")).isFalse();
+        assertThat(StringRegularExpression.isDigit2("123b")).isFalse();
+        assertThat(StringRegularExpression.isDigit2("abc123")).isFalse();
+    }
+
+
+    @Test
+    public void testIsDigit3() {
+
+        assertThat(StringRegularExpression.isDigit3("123")).isTrue();
+        assertThat(StringRegularExpression.isDigit3("40")).isTrue();
+        assertThat(StringRegularExpression.isDigit3("-123")).isFalse();
+        assertThat(StringRegularExpression.isDigit3("123.12")).isFalse();
+        assertThat(StringRegularExpression.isDigit3("123b")).isFalse();
+        assertThat(StringRegularExpression.isDigit3("abc123")).isFalse();
+    }
+
+
+    @Test
+    public void testIsStringOnlyAlphabet() {
+
+        assertThat(StringRegularExpression.isStringOnlyAlphabet("123")).isFalse();
+        assertThat(StringRegularExpression.isStringOnlyAlphabet("a23")).isFalse();
+        assertThat(StringRegularExpression.isStringOnlyAlphabet("abc")).isTrue();
+        assertThat(StringRegularExpression.isStringOnlyAlphabet("ABC")).isTrue();
+        assertThat(StringRegularExpression.isStringOnlyAlphabet("Abc")).isTrue();
+
+    }
+
+
+}
