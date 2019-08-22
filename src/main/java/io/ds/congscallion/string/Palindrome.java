5498d24d85c7df688320c62f9dea64c33492cfd8
wangcong <wangcong@bbdservice.com>
2019-08-22 10:00:03 +0800

    palindrome demo

---
+++ src/main/java/io/ds/congscallion/string/Palindrome.java
@@ -0,0 +1,37 @@
+/**
+ * BrandBigData.com Inc. 
+ * Copyright (c) 2019 All Rights Reserved.
+ */
+package io.ds.congscallion.string;
+
+/**
+ *
+ * 检查给定的字符是否是回文
+ *
+ * 如果String的反向等于自身，则称一个字符串是回文
+ * 例如“aba”是回文，因为“aba”的反向也是“aba”，但是“abc”不是回文，因为“ abc“是”cba“这是不相等的
+ *
+ *
+ * @author wangcong
+ * @version $Id: Palindrome.java, v0.1 8/22/19 9:51 AM wangcong Exp $$
+ */
+public class Palindrome {
+
+
+    public static boolean isPalindrome(String word) {
+
+        return word.equals(reverse(word));
+    }
+
+
+    public static String reverse(String word) {
+
+        if (word.length() < 2) {
+            return word;
+        }
+
+        return reverse(word.substring(1)) + word.charAt(0);
+    }
+
+
+}
---
+++ src/test/java/io/ds/congscallion/string/PalindromeTest.java
@@ -0,0 +1,39 @@
+/**
+ * BrandBigData.com Inc. 
+ * Copyright (c) 2019 All Rights Reserved.
+ */
+package io.ds.congscallion.string;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+/**
+ *
+ *
+ * @author wangcong
+ * @version $Id: PalindromeTest.java, v0.1 8/22/19 9:55 AM wangcong Exp $$
+ */
+public class PalindromeTest {
+
+    @Test
+    public void testReverse() {
+
+        assertThat(Palindrome.reverse("abcd")).isEqualTo("dcba");
+        assertThat(Palindrome.reverse("qwertyuiop")).isEqualTo("poiuytrewq");
+
+    }
+
+
+    @Test
+    public void testPalindrome() {
+
+        assertThat(Palindrome.isPalindrome("abba")).isTrue();
+        assertThat(Palindrome.isPalindrome("abbc")).isFalse();
+        assertThat(Palindrome.isPalindrome("ioooi")).isTrue();
+        assertThat(Palindrome.isPalindrome("iooo1")).isFalse();
+        assertThat(Palindrome.isPalindrome("")).isTrue();
+
+    }
+
+}
